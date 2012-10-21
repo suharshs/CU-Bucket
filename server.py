4c3ca5824373485dd550d5417fadf07b725f6846
suharshs <suharshs@gmail.com>
2012-10-20 22:33:01 -0500

    this better work

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import logging
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
-from urlparse import *
+import urlparse
 from tornado.database import Connection
 from tornado.options import options, define
 
@@ -13,13 +13,13 @@ from handlers.login import *
 from handlers.user import *
 from handlers.signup import *
 
-uses_netloc.append('mysql')
+urlparse.uses_netloc.append('mysql')
 
 PORT = sys.argv[1]
 
 DATABASE_URL = sys.argv[2]
 print DATABASE_URL
-url = urlparse(DATABASE_URL)
+url = urlparse.urlparse(DATABASE_URL)
 
 db = Connection(host=url.hostname, user=url.username, password=url.password, database=url.path[1:])
 
