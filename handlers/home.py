84cc838c008e7db3f3ee7d4d0bf6b0f13b4c1b02
suharshs <suharshs@gmail.com>
2012-10-26 02:21:44 -0500

    fixed query that checks whether the user has alreaded upvoted an activity

--- handlers/home.py
+++ handlers/home.py
@@ -3,7 +3,7 @@ from base import BaseHandler
 
 class HomeHandler(BaseHandler):
     def get(self):
-        sql = """ SELECT * FROM Activity LEFT JOIN (SELECT * FROM UserInterest WHERE userName=\'%s\') as currUserInterest ON Activity.creator = currUserInterest.userName ORDER BY Activity.ID DESC LIMIT 0, 20""" % (self.get_current_user())
+        sql = """ SELECT * FROM Activity LEFT JOIN (SELECT * FROM UserInterest WHERE userName='%s') as currUserInterest ON Activity.ID = currUserInterest.activityID ORDER BY Activity.ID DESC LIMIT 0, 20""" % (self.get_current_user())
         info = {}
         info['results'] = self.application.db.query(sql)
         info['username'] = self.get_current_user()
