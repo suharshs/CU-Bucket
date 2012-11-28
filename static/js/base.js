e6ddef7dd5565653fc2c36399342b54e3cda0efd
mpwoz <martin.woz@gmail.com>
2012-11-27 20:09:30 -0600

    Moved home scripts to base file (so they're available on all pages)

--- static/js/base.js
+++ static/js/base.js
@@ -22,6 +22,32 @@ $(document).ready(function(){
             }
         });
     });
+
+
+    // create the function for the button rating
+    $(".add-to-my-bucket").live("click", function(){
+        var toRemove = $(this);
+        console.log("click the add to my bucket button");
+        $.ajax({
+            type: 'GET',
+            url:  '/activity/add/' + toRemove.parent().attr('id'),
+            success: function(){
+                toRemove.remove();
+            }
+        });
+    });
+
+    $(".delete-button").live("click", function(){
+        var toRemove = $(this).parent();
+        console.log("click the close button");
+        $.ajax({
+            type: 'GET',
+            url:  '/activity/delete/' + toRemove.attr('id'),
+            success: function(){
+                toRemove.remove();
+            }
+        });
+    });
 });
 
 
