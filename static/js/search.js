f90b3ac2a729681b82938af032c547b87a3adeb8
Greg Blaszczuk <maxbla0@gmail.com>
2012-11-28 03:48:49 -0600

    added the search with the suggestion drop down, as well as implemented up/down arrow selection

--- static/js/search.js
+++ static/js/search.js
@@ -1,15 +1,58 @@
 
 $(document).ready(function(){
-    $("#search").keyup(function(){
+    var position=0;
+    var suggestionsNum=2
+    $(this).keydown(function(e){
+        //on down arrow click
+        if(e.keyCode == 40){
+            if(position!=suggestionsNum){
+                if(position!=0){
+                 $("[id*=suggestion_"+position+"]").css('background-color', 'white');   
+                }
+                position=position+1;
+              $("[id*=suggestion_"+position+"]").css('background-color', '#eee9e9');
+              
+           
+            }
+            return true;
+        } 
+        //on up arrow click
+        if(e.keyCode == 38){
+            if(position!=0){
+                if(position!=suggestionsNum+1){
+                 $("[id*=suggestion_"+position+"]").css('background-color', 'white');   
+                }
+                position=position-1;
+              $("[id*=suggestion_"+position+"]").css('background-color', '#eee9e9');
+              
+           
+            }
+            return true;
+        } 
+
+        if(e.keyCode == 13){
+            // user has highlighted a choice
+            if(position!=0){
+                alert($("[id*=suggestion_"+position+"_link]").text());
+            }
+        }
+    });
+
+    $("#searchInput").keyup(function(){
+        if(!$(this).val()){
+            $('.suggestions').css('display', 'none');
+            return;    
+        }
         $.ajax({
             type: 'POST',
             url:  '/search',
             data: {
-                user_string: $("#search").val(),
+                user_string: $("#searchInput").val(),
                 _xsrf: getCookie("_xsrf")
             },
             success: function(response){
                 console.log(response.closest_word);
+                renderSuggestion(response.closest_word);
             }
         });
     });
@@ -18,4 +61,10 @@ $(document).ready(function(){
 function getCookie(name) {
     var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
     return r ? r[1] : undefined;
+}
+
+function renderSuggestion(suggestion){
+    $('#suggestion_1_link').text(suggestion);
+    $('.suggestions').css('display', 'block');
+
 }
