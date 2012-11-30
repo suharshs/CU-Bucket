313a9f5a979d4fe53a5cfafb602ba34718633659
mpwoz <wozniew1@illinois.edu>
2012-11-29 19:12:04 -0600

    Broke up giant sql line

--- handlers/home.py
+++ handlers/home.py
@@ -3,8 +3,20 @@ from base import BaseHandler
 
 class HomeHandler(BaseHandler):
     def get(self):
-        sql = """ SELECT * FROM Activity LEFT JOIN (SELECT userName as interestUserName, activityID FROM UserInterest WHERE userName='%s') as currUserInterest ON Activity.ID = currUserInterest.activityID LEFT JOIN (SELECT userName as completedUserName, activityID FROM UserCompleted WHERE userName='%s') as currUserComplete ON Activity.ID = currUserComplete.activityID ORDER BY Activity.ID DESC LIMIT 0, 20""" % (self.get_current_user(), self.get_current_user)
         info = {}
-        info['results'] = self.application.db.query(sql)
         info['username'] = self.get_current_user()
+
+        sql = """ SELECT * FROM Activity 
+        LEFT JOIN (SELECT userName as interestUserName, 
+        activityID FROM UserInterest WHERE userName='{0}') 
+        AS currUserInterest ON 
+        Activity.ID = currUserInterest.activityID 
+        LEFT JOIN (SELECT userName as completedUserName, activityID 
+            FROM UserCompleted WHERE userName='{0}') 
+        AS currUserComplete 
+        ON Activity.ID = currUserComplete.activityID 
+        ORDER BY Activity.ID DESC 
+        LIMIT 0, 20""".format(self.get_current_user())
+
+        info['results'] = self.application.db.query(sql)
         self.render('home.html', info=info)
