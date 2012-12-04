be44b0a9b4d81d023ccf44ddc5d74f6c7d84d424
suharshs <suharshs@gmail.com>
2012-12-04 17:11:17 -0600

    added complete button support for top page

--- handlers/top.py
+++ handlers/top.py
@@ -3,7 +3,16 @@ from base import BaseHandler
 
 class TopHandler(BaseHandler):
     def get(self):
-        sql = """ SELECT * FROM Activity LEFT JOIN (SELECT * FROM UserInterest WHERE userName='%s') as currUserInterest ON Activity.ID = currUserInterest.activityID ORDER BY Activity.rating DESC LIMIT 0, 20""" % (self.get_current_user())
+        sql = """ SELECT * FROM Activity 
+        LEFT JOIN (SELECT userName as interestUserName, activityID 
+            FROM UserInterest WHERE userName='{0}') 
+        AS currUserInterest ON 
+        Activity.ID = currUserInterest.activityID 
+        LEFT JOIN (SELECT userName as completedUserName, activityID 
+            FROM UserCompleted WHERE userName='{0}') 
+        AS currUserComplete 
+        ON Activity.ID = currUserComplete.activityID 
+        ORDER BY Activity.rating DESC LIMIT 0, 20""".format(self.get_current_user())
         info = {}
         info['results'] = self.application.db.query(sql)
         info['username'] = self.get_current_user()
